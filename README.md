4386be3daaf4787d7390dde433c0e1eb6b8f440b
aerome2 <22zakdav@astonfields.worcs.sch.uk>
2026-06-30 14:40:32 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# newstoreaerome
+# aeromestore
---
+++ donate_1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html><html lang="en" class="donation-banner-iframe"><head>
+<meta charset="utf-8"><title>Donate</title><style>body{margin:0}</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="js/jquery-1.10.2.min.js"></script>
+      <script src="js/polyfill.min.js"></script>
+      <script src="js/athena_1.js"></script>
+          <script src="js/donation-banner.min.js" type="text/javascript"></script>
+    </head>
+    <body class="js-ia-iframe" style="margin:0">
+          <link href="css/banner.css" rel="stylesheet" type="text/css">
+    <link href="css/chevron.css" rel="stylesheet" type="text/css">
+      <header id="donate_banner" class="js-donate-banner formdesign-psa" itemscope="" itemtype="http://schema.org/WPAdBlock" data-donationsourcedata="ctx=wb;uid=0b22a99558a34e317623324e248c24ab;ref=https%3A%2F%2Fweb.archive.org%2Fweb%2F20260310153115%2Fhttps%3A%2F%2Fwww.clarity.ms%2Ftag%2Ffxlepb8eap" data-exp="20260601JuneDeploy2" data-variant="WBjuneGFMPvanishPSA" data-platform="wb" data-baseurl="https://archive.org" data-bannertemplate="psa" data-bannerbackground="default" data-snowfall="off" data-selectedtextcolor="#000000" data-selectedfillcolor="#F9BF3B" data-paymentoptionfillcolor="" data-paymentoptiontextcolor="" data-formbgcolor="#ffffff" data-formtextcolor="#000000" data-donatepageselectedtextcolor="#000000" data-donatepageselectedfillcolor="#F9BF3B" data-psabannerdestinationurl="https://donate.archive.org/campaign/788361/donate?c_src=gofundmeprobanner-01062026WBvanishPSA" data-variantdollaramounts="[10, 15, 50]" data-defaultselectedamount="15" data-debugmode="false" style="--donationBannerChevronBG: #F9BF3B;  ">
+                  <div class="ia-banner visible psa">
+            <div class="appeal-text" style="--donationBannerAppealTextColor: #000000;">
+              <div class="desktop-body-content"><b>Please Don't Scroll Past This</b> Can you chip in? The Internet Archive, which runs this project, partners with institutions across the globe to preserve cultural heritage that would otherwise be lost forever.<b> We need your support in continuing this vital work.</b></div>
+              <div class="mobile-body-content"><b>Please Don't Scroll Past This</b> Can you chip in? The Internet Archive, which runs this project, partners with institutions across the globe to preserve cultural heritage that would otherwise be lost forever.<b> We need your support in continuing this vital work.</b></div>
+            </div>
+            <form id="donate-close-button" class="banner-close" aria-label="Close banner">
+              <span class="sr-only">Close banner</span>
+              <fieldset>
+                <button aria-label="Close Banner" type="submit"></button>
+              </fieldset>
+            </form>
+            <div style="display: none;">      <div class="donation-form-container">
+        <donation-form-edit-donation amountoptions="[10, 15, 50]" defaultselectedamount="15" amounttitledisplaymode="slot" customamountmode="hide" customfeescheckboxmode="hide" frequencyselectionmode="hide" stepnumbermode="hidenumbers" class="minimal-widget focus-on-child-only" style="--paymentButtonSelectedFontColor: #000000;--paymentButtonSelectedColor: #F9BF3B;--formSectionTextColor: #000000;">
+          <div slot="edit-donation-amount-title">
+            <p style="font-size: 1.8rem; line-height: calc(1.2rem * 2); margin: 0;"><b>Can You Chip In?</b> (USD)</p>
+          </div>
+        </donation-form-edit-donation>
+      </div>
+    </div>
+          </div>
+              </header>
+          
+  
+</body></html>
